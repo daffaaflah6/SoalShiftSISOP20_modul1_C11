db0150d708f3da0b58a0aa1dabf0fac4cff85e4b
Muhammmad Daffa' Aflah Syarif <52326074+daffaaflah6@users.noreply.github.com>
2020-02-20 17:45:10 +0700

    Update soal1a.sh

--- soal1/soal1a.sh
+++ soal1/soal1a.sh
@@ -1,1 +1,1 @@
-awk -F "," 'NR > 1 {r[$13]+=$21} END {for(x in r) print (x, r[x])}' Sample-Superstore.tsv | sort -g | head -1
+awk -F "," 'NR > 1 {r[$13]+=$21} END {for(x in r) print (x, r[x])}' Sample-Superstore.tsv | sort -g | head -n 1
