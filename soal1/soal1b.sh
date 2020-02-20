772f9a1241395e4000f436c85756cb99fdb45c23
Muhammmad Daffa' Aflah Syarif <52326074+daffaaflah6@users.noreply.github.com>
2020-02-20 17:46:00 +0700

    Update soal1b.sh

--- soal1/soal1b.sh
+++ soal1/soal1b.sh
@@ -1,1 +1,1 @@
-awk -F "," 'NR > 1 {if($13=="Central"){s[$11]+=$21}} END {for(x in s) print (s[x], x)}' Sample-Superstore.tsv | sort -g | head -2
+awk -F "," 'NR > 1 {if($13=="Central"){s[$11]+=$21}} END {for(x in s) print (s[x], x)}' Sample-Superstore.tsv | sort -g | head -n 2
