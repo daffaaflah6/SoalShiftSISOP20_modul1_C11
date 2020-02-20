005a89d096adb1a13d14147d78ab377ae6eacf18
Muhammmad Daffa' Aflah Syarif <52326074+daffaaflah6@users.noreply.github.com>
2020-02-21 03:49:36 +0700

    Update soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -4,7 +4,7 @@ echo $a
 echo "--------------------------------"
 echo "	"
 echo "State dengan Profit minimum : "
-b=$(awk -F "," 'NR > 1 {if($13=="Central"){s[$11]+=$21}} END {for(x in s) print s[x] " " x "/"}' Sample-Superstore.tsv | sort -g | head -n 2)
+b=$(awk -F "," 'NR > 1 {if($13 == "Central"){s[$11]+=$21}} END {for(x in s) print s[x] " " x "/"}' Sample-Superstore.tsv | sort -g | head -n 2)
 echo $b
 echo "--------------------------------"
 echo "	"
