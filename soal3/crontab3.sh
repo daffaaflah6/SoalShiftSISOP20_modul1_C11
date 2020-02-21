46e02fa27df5f88aebde3d8e1c30dcd06e91550b
marshanabilah <marsha.nabilah@gmail.com>
2020-02-21 15:28:17 +0700

    5 6,8,14 * * 1-5,7 /bin/bash /home/bela/modul1/soal3/soal3a.sh

--- soal3/crontab3.sh
+++ soal3/crontab3.sh
@@ -1,1 +1,1 @@
-5 6,14,22 1-5,7 * * ls /home/syarif/Downloads/sisop20/shift_modul1/soal3/soal3a.sh > /home/syarif/Downloads/sisop20/shift_modul1/soal3/kenangan
+5 6,8,14 * * 1-5,7 /bin/bash /home/bela/modul1/soal3/soal3a.sh
