cd279e9a056633778b1e69acf6d50bd8d8ca95fd
marshanabilah <58138825+marshanabilah@users.noreply.github.com>
2020-02-21 07:49:14 +0700

    Update soal3a.sh

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,4 +1,6 @@
 for no in 1 .. 28
 do
-  wget -O "pdkt_kusuma_$no" "https://loremflickr.com/320/240/cat" -a "wget.log"
+  wget -O "pdkt_kusuma_$no" "https://loremflickr.com/320/240/cat" --append-output wget.log
 done
+
+grep "Location" wget.log > location.log
